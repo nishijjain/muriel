64ab30d8f35f1e333c58e37c2b7e649b3836bf90
Nishi Jain <nishijjain2000@gmail.com>
2020-10-07 23:45:23 +0530

    products page updated

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
     $(this).scrollTop(0);
-    AOS.init();
+    AOS.init();   
+
+    $(".content").hide();
+    $(".show_hide").on("click", function () {
+        var txt = $(".content").is(':visible') ? 'Read More >>' : 'Read Less <<';
+        $(".show_hide").text(txt);
+        $(this).next('.content').slideToggle(200);
+    });
 });
 
