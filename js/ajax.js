1fd19c69dd0863330afd0339b7a551ac5b43e187
Nishi Jain <nishijjain2000@gmail.com>
2020-10-23 22:00:28 +0530

    all products added.

--- js/ajax.js
+++ js/ajax.js
@@ -15,7 +15,7 @@ function loadSites() {
                     if (i === 0) {
                         $('#links').append(`
                         <div class="justify-content-center col-12 col-md-6 col-xl-4 my-4 mx-md-0">
-                            <div class="card product-scrollable card-body h-100">
+                            <div class="card product-scrollable card-body h-100" >
                             <img src="${obj[i].image}" alt="" style="width: 280px; height: 280px;"> 
                             <p><strong><center>${obj[i].name}</center></strong></p>
                             <p><strong>Skin Type : </strong> ${obj[i].skintype}</p>
@@ -28,7 +28,7 @@ function loadSites() {
                     } else {
                         $('#links').append(`
                         <div class="justify-content-center col-12 col-md-6 col-xl-4 my-4 mx-md-0">
-                            <div class="card product-scrollable card-body h-100">
+                            <div class="card product-scrollable card-body h-100" >
                             <img src="${obj[i].image}" alt="" style="width: 280px; height: 280px;">
                             <p><strong><center>${obj[i].name}</center></strong></p>
                             <p><strong>Skin Type : </strong> ${obj[i].skintype}</p>
@@ -37,7 +37,43 @@ function loadSites() {
                             <p><strong>Ingredients : </strong> ${obj[i].ingredients}</p>
                             </div>
                         </div>
+                        `)
+                    }
+                }
+            }
+        }
+    }
 
+    xh.onload = function () {
+        if (this.status == 200) {
+            var obj = JSON.parse(this.responseText)
+            if (obj.length == 0) {
+                $('#link').append(`
+               <p>No Links Available. Please add a few links in users.json to get started.</p>
+               `)
+            } else {
+                for (var i = 0; i < obj.length; i++) {
+                    if (i === 0) {
+                        $('#link').append(`
+                        <div class="justify-content-center col-12 col-md-6 col-xl-4 my-4 mx-md-0">
+                            <div class="card product-scrollable card-body h-100" >
+                            <img src="${obj[i].image}" alt="" style="width: 280px; height: 280px;"> 
+                            <p><strong><center>${obj[i].name}</center></strong></p>
+                            <p><strong>Skin Type : </strong> ${obj[i].skintype}</p>
+                            <p><strong>Ingredients : </strong> ${obj[i].ingredients}</p>
+                            </div>
+                        </div>
+                        `)
+                    } else {
+                        $('#link').append(`
+                        <div class="justify-content-center col-12 col-md-6 col-xl-4 my-4 mx-md-0">
+                            <div class="card product-scrollable card-body h-100" >
+                            <img src="${obj[i].image}" alt="" style="width: 280px; height: 280px;">
+                            <p><strong><center>${obj[i].name}</center></strong></p>
+                            <p><strong>Skin Type : </strong> ${obj[i].skintype}</p>
+                            <p><strong>Ingredients : </strong> ${obj[i].ingredients}</p>
+                            </div>
+                        </div>
                         `)
                     }
                 }
@@ -45,3 +81,4 @@ function loadSites() {
         }
     }
 }
+
